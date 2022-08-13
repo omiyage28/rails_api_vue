b69c2a1925d9b63f1f1846ca2edd6631453aaab5
omiyage28 <90824672+omiyage28@users.noreply.github.com>
2022-08-13 12:44:55 +0900

    マイクロポスト　フロントエンド

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,8 @@
 import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+require("plugins/dayjs")
+
 import Vue from 'vue'
 import App from '../app.vue'
 import Vuetify from 'vuetify'
