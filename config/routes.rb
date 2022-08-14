5e58524b1f931db3dfbdd8db1aedf60d0024d59d
omiyage28 <90824672+omiyage28@users.noreply.github.com>
2022-08-14 12:54:17 +0900

    プロフィール編集　バックエンド

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,12 @@ Rails.application.routes.draw do
   get '/index', to: 'home#index'
   
   namespace :api do
-    resources :users, only: [:create]
+    resources :users, only: [:create, :show]
     resource :session, only: [:create, :destroy]
     resources :microposts, only: [:index, :create, :update, :destroy, :show]
+  end
+
+  namespace :me do
+    resource :account, only: [:update]
   end
 end
