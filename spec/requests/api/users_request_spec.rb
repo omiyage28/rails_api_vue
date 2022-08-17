165e966ef82645b4b9489fd9f11645254a8a1b09
omiyage28 <90824672+omiyage28@users.noreply.github.com>
2022-08-17 12:49:13 +0900

    ユーザー絞り込み　バックエンド

--- spec/requests/api/users_request_spec.rb
+++ spec/requests/api/users_request_spec.rb
@@ -15,6 +15,7 @@ RSpec.describe "Api::Users", type: :request do
           include(
               'id' => user.id,
               'name' => user.name,
+              'tags' => be_a(Array)
           )
         })
       end
